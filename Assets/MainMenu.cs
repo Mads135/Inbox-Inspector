4ba4b465acfa9a7e3b73b83f859248f43e7667b3
Mads135 <mads.thorsgaard7@gmail.com>
2025-01-15 10:17:01 +0100

    MainMenu + Start og quit knap virker

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton( )   {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+    public void StartGame() {
+SceneManager.LoadScene("Game");
+    }
+}
